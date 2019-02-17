540444b5d3e154e3dd0ce2705d49aa805667db0a
Anthony OTHMANI <anthony.othmani@gmail.com>
2019-02-17 19:20:16 +0100

    dd

--- tme1/main.c
+++ tme1/main.c
@@ -1,14 +1,20 @@
 #include <mpi.h>
 #include <stdio.h>
+#include "mpi_server.h"
+
+
+
 
 int main( int argc, char** argv){
   int rang;
   int size;
   
   
-  MPI_Init(&argc, &argv);
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, NULL);
   MPI_Comm_rank(MPI_COMM_WORLD, &rang);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  
   
   printf("Processus %d sur %d : Hello MPI \n", rang, size);
   MPI_Finalize();
