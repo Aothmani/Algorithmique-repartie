4df51ead8f52fbc09606074633a0543205e0cb92
OTHMANI ANTHONY <3200072@ppti-14-508-09.ufr-info-p6.jussieu.fr>
2019-02-22 13:42:36 +0100

    tp1 bug

--- tme1/main.c
+++ tme1/main.c
@@ -3,32 +3,36 @@
 #include "mpi_server.h"
 
 char buf[100];
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond_msg_recu = PTHREAD_COND_INITIALIZER;
 
-char* receive(int tag, int source){
-  int status;
-  buf = MPI_Receive(buf, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-  return status;
+void attenton(int tag, int source){
+  MPI_Status status;
+  MPI_Recv(buf, 20, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
+  pthread_mutex_lock(&mutex);
+  pthread_cond_broadcast(&cond_msg_recu);
+  pthread_mutex_unlock(&mutex);
 }
 
 int main( int argc, char** argv){
-  int rang;
+  printf("totototototototototo");
+  int rank;
   int size;
-    
   MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, NULL);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rang);
+  printf("totototototototototo");
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
+  start_server(attenton);
+  
+  sprintf(buf, "Hello %d from %d\0", (rank+1)%size, rank);
+   
+  MPI_Send(buf,20, MPI_CHAR, (rank+1)%size, 0,MPI_COMM_WORLD);
+  pthread_mutex_lock(&mutex);
+  pthread_cond_wait(&cond_msg_recu, &mutex);
+  pthread_mutex_unlock(&mutex);
+  printf("%s\n",buf);
 
-  sprintf(message, "Hello %d from %d", (rank+1)%size, rank);
-    
-  if (rank != size-1){
-    MPI_Send(message, strlen(message)+1, MPI_CHAR, (rank+1)%size, 0,MPI_COMM_WORLD);
-    MPI_Recv(message, BSIZE, MPI_CHAR, (rank-1)%size, 0, MPI_COMM_WORLD, &status);
-    printf("%s\n",message);
-  }else{
-    MPI_Recv(msg, BSIZE, MPI_CHAR, (rank-1)%size, 0, MPI_COMM_WORLD, &status);
-    printf("%s\n",msg);
-    MPI_Send(message, strlen(message)+1, MPI_CHAR, (rank+1)%size, 0, MPI_COMM_WORLD);
-  }
+  destroy_server();
   
   MPI_Finalize();
   return 0;
